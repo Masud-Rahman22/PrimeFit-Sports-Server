8e7a1897f441cff0fa5ee955602e1440f5c56707
Masud-Rahman22 <masudrhmn6560@gmail.com>
2024-09-21 16:08:48 +0600

    product is created successfully and fixed statusCode in response

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -4,7 +4,7 @@ import sendResponse from '../../utils/sendResponse';
 import { ProductServices } from './product.service';
 
 const createProduct = catchAsync(async (req, res) => {
-    const {productInfo} = req.body;
+    const productInfo = req.body;
   const result = await ProductServices.createProductIntoDB(productInfo)
   sendResponse(res, {
     statusCode: httpStatus.OK,
--- src/app/utils/sendResponse.ts
+++ src/app/utils/sendResponse.ts
@@ -1,18 +1,21 @@
-import { Response } from "express";
+import { Response } from 'express';
 
 type TResponse<T> = {
-    statusCode: number,
-    success: boolean,
-    message?: string,
-    data: T
-}
+  statusCode: number;
+  success: boolean;
+  message?: string;
+  data: T;
+};
 
-const sendResponse = <T>(res: Response, data: TResponse<T>) => {
-    res.status(data?.statusCode).json({
-        success: data.success,
-        message: data.message,
-        data: data.data
-    })
-}
+const sendResponse = <T>(res: Response, response: TResponse<T>) => {
+  const { statusCode, success, message, data } = response;
+
+  res.status(statusCode).json({
+    success,
+    statusCode,
+    message,
+    data,
+  });
+};
 
 export default sendResponse;
