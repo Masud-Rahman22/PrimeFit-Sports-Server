3bf90d1ae4eea875337d227312cee68c6759337c
Masud-Rahman22 <masudrhmn6560@gmail.com>
2024-09-22 02:09:47 +0600

    fixed some issues

---
+++ src/app/modules/product/product.interface.ts
@@ -0,0 +1,11 @@
+export interface IProduct {
+    name: string;
+    description: string;
+    category: string;
+    brand: string;
+    stock: number;
+    rating: number;
+    price: number;
+    image: string;
+  }
+  
