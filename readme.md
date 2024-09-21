81a95343b03d7d87fb519b7b5b13e31b8107d59e
Masud-Rahman22 <masudrhmn6560@gmail.com>
2024-09-21 14:45:16 +0600

    first commit

---
+++ readme.md
@@ -0,0 +1,17 @@
+## README: Express Application with TypeScript, ESLint, and Prettier
+
+This project is an Express application written in TypeScript and utilizes ESLint and Prettier for code linting and formatting.
+
+**Prerequisites:**
+
+- Node.js (version 16 or later recommended): https://nodejs.org/en/about/previous-releases
+- npm (comes bundled with Node.js)
+
+**Installation:**
+
+1. Clone this repository or download the project files.
+2. Open a terminal in the project directory.
+3. Install dependencies:
+
+   ```bash
+   npm install
---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
+import router from './app/routes';
+import cookieParser from 'cookie-parser';
+const app: Application = express();
+
+// parsers
+app.use(express.json());
+app.use(cookieParser())
+app.use(cors({ origin: ['http://localhost:5173'] }));
+
+app.use('/api/v1', router)
+
+const test = (req: Request, res: Response) => {
+  const a = 10;
+  res.sendStatus(a)
+}
+
+app.get('/', test);
+
+app.use(globalErrorHandler)
+app.use(notFound)
+export default app;
+// console.log(process.cwd())
