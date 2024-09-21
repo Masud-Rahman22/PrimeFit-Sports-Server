b1f22cfd8fe57f8896302b375919397a1fea8dc4
Masud-Rahman22 <masudrhmn6560@gmail.com>
2024-09-21 15:35:29 +0600

    product route is added with post method

---
+++ src/app/modules/product/product.controller.ts
@@ -0,0 +1,19 @@
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { ProductServices } from './product.service';
+
+const createProduct = catchAsync(async (req, res) => {
+    const {productInfo} = req.body;
+  const result = await ProductServices.createProductIntoDB(productInfo)
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Rentals retrieved successfully',
+    data: result,
+  });
+});
+
+export const ProductControllers = {
+  createProduct
+};
